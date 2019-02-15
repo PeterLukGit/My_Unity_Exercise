e728dad75742cc12b8cb08f6ee9a4ce69a6dd59e
PeterLukGit <peter33633@gmail.com>
2019-02-16 00:25:11 +0800

    0

---
+++ 2017Particle System/2017.3 Example Project - Particle System Improvements/Assets/Ribbons/Scripts/ScrollTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour {
+
+    private Material mat;
+    private int texOffset;
+    public Vector2 offset = new Vector2(1, 0);
+    public float speed = 1f;
+    private Vector2 curOffset;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<ParticleSystemRenderer>().trailMaterial;
+        texOffset = Shader.PropertyToID("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        curOffset += offset * Time.deltaTime * speed;
+        mat.SetTextureOffset(texOffset, curOffset);
+	}
+}
